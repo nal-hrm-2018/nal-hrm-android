d3492b6ce65ad0a2d1f27ed6efc5c7eb18e04786
LuuNgocLan <luungoclan125690@gmail.com>
2018-07-31 16:13:36 +0700

    implement profile api

--- app/src/main/java/com/dal/hrm_management/api/ApiInterface.java
+++ app/src/main/java/com/dal/hrm_management/api/ApiInterface.java
@@ -1,10 +1,12 @@
 package com.dal.hrm_management.api;
 
 import com.dal.hrm_management.models.LoginModel;
+import com.dal.hrm_management.models.profile.ProfileResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
@@ -14,4 +16,8 @@ public interface ApiInterface {
     Call<LoginModel> getToKen(@Header("Authorization") String auth,
                               @Field("email") String email,
                               @Field("password") String password);
+
+    @GET("api/profile/show")
+    Call<ProfileResponse> getProfile(@Header("Authorization") String auth,
+                                     @Header("token") String token);
 }
