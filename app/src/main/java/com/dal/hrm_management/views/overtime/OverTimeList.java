3659bb4d2dae393bb5e365a2868d3e187f7c795c
MrTus97 <mr.tus97@gmail.com>
2018-09-05 15:33:47 +0700

    done design ui overtime list personal and fake data for it

--- app/src/main/java/com/dal/hrm_management/views/overtime/OverTimeList.java
+++ app/src/main/java/com/dal/hrm_management/views/overtime/OverTimeList.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.dal.hrm_management.R;
+import com.dal.hrm_management.adapter.listOT.FakeDataForListOvertime;
+import com.dal.hrm_management.adapter.listOT.OvertimeListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class OverTimeList extends Fragment{
     //In layout
@@ -20,13 +27,37 @@ public class OverTimeList extends Fragment{
     private FloatingActionButton fabAdd;
     private RecyclerView rvOvertime;
 
+    private RecyclerView.LayoutManager layoutManager;
+
     private View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_overtime_list_for_personel, container, false);
         init();
+        fakeData();
         return view;
+    }
+
+    private void fakeData() {
+        rvOvertime.setVisibility(View.VISIBLE);
+        prgShowMore.setVisibility(View.GONE);
+        List<FakeDataForListOvertime> list = new ArrayList<FakeDataForListOvertime>();
+        for (int i = 0 ; i < 5;i++){
+            FakeDataForListOvertime data =
+                    new FakeDataForListOvertime(UUID.randomUUID().toString(),
+                            UUID.randomUUID().toString(),
+                            UUID.randomUUID().toString(),
+                            UUID.randomUUID().toString(),
+                            UUID.randomUUID().toString(),
+                            UUID.randomUUID().toString(),
+                            UUID.randomUUID().toString(),
+                            UUID.randomUUID().toString());
+            list.add(data);
+        }
+        OvertimeListAdapter adapter = new OvertimeListAdapter(getContext(),list);
+        rvOvertime.setLayoutManager(layoutManager);
+        rvOvertime.setAdapter(adapter);
     }
 
     private void init() {
@@ -37,5 +68,7 @@ public class OverTimeList extends Fragment{
         fabAdd = view.findViewById(R.id.fabOvertimeListFra_add);
         rvOvertime = view.findViewById(R.id.rvOvertimeListFra_List);
         prgShowMore = view.findViewById(R.id.prgOvertimeListFra_ShowMore);
+
+        layoutManager = new LinearLayoutManager(getActivity());
     }
 }
